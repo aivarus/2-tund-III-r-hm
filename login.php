f8bfd2cc993d45eb942e6fb837c72d7464a7965f
Aivar Russki <aivarus@greeny.cs.tlu.ee>
2015-09-09 12:39:27 +0300

    first lines of code

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+		// LOGIN.PHP
+		
+?>
+<html>
+<head>
+		<title>login page</title>
+</head>
+<body>
+		
+		<h2>log in</h2>
+		
+			<input type="email" placeholder="e-post"> <br><br>
+			<input type="password" placeholder="parool">  <br><br>
+			<input type="submit" value="log in">
+		
+			
+			
+		<h2>Create user</h2>
+</body>
+</html>
+
