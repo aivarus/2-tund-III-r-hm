e18f447510aa7107d7aa7cbda750d3b4835f8f12
Aivar Russki <aivarus@greeny.cs.tlu.ee>
2015-09-09 12:17:17 +0300

    first lines of code

--- hello.php
+++ hello.php
@@ -4,3 +4,39 @@
 
 				echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+
+				$age = 10;
+				
+				//testin loogikat, juhul kui vanus on väiksem 
+				//kui 18, kirjuta ""alaealine" , muul juhul "täisealine"
+				
+				//if'i sisse loogikatehe
+				if($age < 18) {
+					//tõene
+					echo "alaealine" ;
+				
+				} else { 
+					//väär
+					echo "täisealine" ;
+				}
+				
+?>
+<br>
+<?php
+				
+				// Vastavalt vanusele trükime niimitu korda välja sõna "palju"
+				
+				for($i = 0; $i < $age; $i = $i + 1) {
+					
+					// tegevus mis kordub
+					echo "palju".$i." ";
+					
+				}
+				
+				echo "õnne!";
+				
+
+?>
+				
